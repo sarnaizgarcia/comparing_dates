4b4adcd4597c4e7c03d2d33bc56f37720669eaa5
Python <python@ZiMac4-2.local>
2019-11-18 21:22:54 +0100

    solución con datetime

---
+++ comparedates.py
@@ -0,0 +1,21 @@
+import datetime
+
+from dummydate import date_to_check
+
+year_to_check = date_to_check.get('year')
+month_to_check = date_to_check.get('month')
+day_to_check = date_to_check.get('day')
+hour_to_check = date_to_check.get('hour')
+minute_to_check = date_to_check.get('minute')
+
+now = datetime.datetime.now()
+input_date = datetime.datetime(year_to_check, month_to_check, day_to_check, hour_to_check, minute_to_check)
+
+def compare_dates(now, input_date):
+    if now >= input_date:
+        return 'Ese momento ya ha pasado'
+    else:
+        return 'Todavía no hemos llegado'
+    
+result = compare_dates(now, input_date)
+print(result)
---
+++ dummydate.py
@@ -0,0 +1,7 @@
+date_to_check = {
+    'year': 2019,
+    'month': 11,
+    'day': 18,
+    'hour': 22,
+    'minute': 3,
+}
