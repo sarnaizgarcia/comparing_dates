dcdfb46c84f8c2d66a2590b8ccba579aa21bd5d0
Python <python@ZiMac4-2.local>
2019-11-18 21:25:52 +0100

    cambio en import

--- comparedates.py
+++ comparedates.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 
 from dummydate import date_to_check
 
@@ -8,8 +8,8 @@ day_to_check = date_to_check.get('day')
 hour_to_check = date_to_check.get('hour')
 minute_to_check = date_to_check.get('minute')
 
-now = datetime.datetime.now()
-input_date = datetime.datetime(year_to_check, month_to_check, day_to_check, hour_to_check, minute_to_check)
+now = datetime.now()
+input_date = datetime(year_to_check, month_to_check, day_to_check, hour_to_check, minute_to_check)
 
 def compare_dates(now, input_date):
     if now >= input_date:
